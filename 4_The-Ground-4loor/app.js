0933a545d46576d917e70c2ffd5ae4b56b76ed69
mirman-mirovenger2016 <mirovenger2016@mirmanmustangs.org>
2015-12-09 15:04:26 -0800

    Fixing 4loor

--- 4_The-Ground-4loor/app.js
+++ 4_The-Ground-4loor/app.js
@@ -3,7 +3,10 @@ $(document).ready(function(){
     event.preventDefault();
     //Use jQuery to collect the values from the fields and create a
     //JSON object containing them. Then, log the result to the console
-
+    var Tagurl  = "http://x.mirman.org:1031/ground4loor"
+    $.post(Tagurl, formAnswers, function(req){
+      console.log(req);
+    })
     // Secondary Objective: Use the AJAX methods built into jQuery to send
     // The form data to http://x.mirman.org:1031/ground4loor. Write a callback to handle
     // the response.
@@ -16,24 +19,3 @@ var formAnswers = {
   email : $("#email").val(),
   message : $("#message").val()
 }
-
-
-var Tagurl  = "http://x.mirman.org:1031/ground4loor"
-$.post(Tagurl, formAnswers, function(req){
-  console.log(req);
-})
-
-
-var $input = $('input:text'),
-    $register = $('#register');
-
-$register.attr('disabled', true);
-$input.keyup(function() {
-    var trigger = false;
-    $input.each(function() {
-        if (!$(this).val()) {
-            trigger = true;
-        }
-    });
-    trigger ? $register.attr('disabled', true) : $register.removeAttr('disabled');
-});
